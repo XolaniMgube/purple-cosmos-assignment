8243c969da5a7655cb9a5ead845b9008a297b13e
XolaniMgube <xnmgube@gmail.com>
2020-07-19 14:13:35 +0200

    added the edit and delete functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var APIEndPoint = "https://test.purplecosmos.co.za/api/api.aspx";
-        let emptyString = "";
+        let tableRows = "";
 
     $(document).ready(function() {
       
@@ -12,15 +12,15 @@ var APIEndPoint = "https://test.purplecosmos.co.za/api/api.aspx";
         success: function (data) {
           for(let i = 0; i < data.d.length; i++) {
             if(data.d[i].Status < 2) {
-              emptyString += `<tr>
+              tableRows += `<tr>
                                 <td>${data.d[i].ClientName}</td>
-                                <td>${data.d[i].JobNumber}</td>
+                                <td>PC000${data.d[i].JobNumber}</td>
                                 <td>${data.d[i].Title}</td>
-                                <td><a href="#">edit</a> / <a href="#">delete</a></td>
+                                <td><a href="./edit-job-details.html">edit</a> / <a  id="btnDeleteJob" href="#">delete</a></td>
                               </tr>`
             } 
           }
-        document.getElementById("myTable").innerHTML = emptyString
+        document.getElementById("myTable").innerHTML = tableRows
         console.log( data.d );
         }
       });
@@ -34,4 +34,30 @@ var APIEndPoint = "https://test.purplecosmos.co.za/api/api.aspx";
             console.log(data.d)
           }
       });
+
+      $.ajax({
+        type: "POST",
+        crossDomain: true,
+        url: APIEndPoint + "/SaveJob",
+        data: JSON.stringify(JobDetails), //Updates job with id 4. If Id = 0 then a new job is added
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+          console.log(data.d)
+        }
+      });
     })
+
+    $(".btnDeleteJob").click(function () {
+
+      $.ajax({
+        type: "POST",
+        crossDomain: true,
+        url: APIEndPoint + "/DeleteJob",
+        data: '{"JobId": 4 }', //Loads job with id 4.
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+          console.log(data)
+        }
+      });
+    });
+
